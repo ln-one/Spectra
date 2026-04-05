2501d4b2f946d27764b41dac75e30782ab18d848
Aictdd <3179629008@qq.com>
2026-04-05 14:40:48 +0800

    Revert "fix(css): 允许目录页显示 Mermaid 图表"
    This reverts commit 5771cee.

--- backend/services/template/css_generator.py
+++ backend/services/template/css_generator.py
@@ -178,7 +178,9 @@ section.density-dense img[alt="Mermaid Diagram"] {
 }
 
 section.cover .mermaid,
-section.cover img[alt="Mermaid Diagram"] {
+section.toc .mermaid,
+section.cover img[alt="Mermaid Diagram"],
+section.toc img[alt="Mermaid Diagram"] {
   display: none;
 }
 """
